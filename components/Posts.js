c674b08b6e70f3cc57d6337f50f51e65d0401ad4
Cristiano Silva <cristiano_own@hotmail.com.br>
2022-08-01 16:49:59 -0300

    Projeto finalizado com sucesso

--- components/Posts.js
+++ components/Posts.js
@@ -1,36 +1,32 @@
-import React from 'react'
+import { collection, onSnapshot, orderBy, query } from '@firebase/firestore';
+import React, { useEffect, useState } from 'react'
+import { db } from '../firebase';
 import Post from "./Post";
 
-const posts = [
-    {
-        id: '123',
-        username: 'ssssangha',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'This is DOPE!',
-    },
-    {
-        id: '123',
-        username: 'ssssangha',
-        userImg: 'https://links.papareact.com/3ke',
-        img: 'https://links.papareact.com/3ke',
-        caption: 'This is DOPE!',
-    },
-
-]
-
-
 function Posts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(
+        () =>
+            onSnapshot(
+                query(collection(db, 'posts'), orderBy('timestamp', 'desc')),
+                (snapshot) => {
+                    setPosts(snapshot.docs);
+                }
+            ),
+        [db]
+    );
+
     return (
         <div>
             {posts.map((post) => (
-                <Post 
-                key={post.id}
-                id={post.id}
-                username={post.username}
-                userImg={post.userImg}
-                img={post.img}
-                caption={post.caption}
+                <Post
+                    key={post.id}
+                    id={post.id}
+                    username={post.data().username}
+                    userImg={post.data().profileImg}
+                    img={post.data().image}
+                    caption={post.data().caption}
                 />
             ))}
         </div>
